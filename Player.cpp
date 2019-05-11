8b59d89ed79762a3af5bd86b295cb5d5f445e470
sancoon <wilson.zhou1941@gmail.com>
2019-05-11 23:34:54 +1000

    +basic data values +unfinished player turn

--- Player.cpp
+++ Player.cpp
@@ -1,11 +1,44 @@
 #include "Player.h"
 
+using namespace std;
 
 
 Player::Player()
 {
 }
 
+Player::Player(char side, string playerName, bool humanP) {
+	name = playerName;
+	player = side;
+	human = humanP;
+}
+
+void Player::makeTurn(Board &board) {
+	if (human) {
+		humanTurn(board);
+	}
+	else {
+		cpuTurn(board);
+	}
+}
+
+void Player::humanTurn(Board &board) {
+	auto timeStart = chrono::steady_clock::now();
+	int eSeconds;
+	do {
+
+		//input handling
+		if (_kbhit()) {
+
+		}
+
+		//calculate delta time since start of turn
+		auto timeNow = chrono::steady_clock::now();
+		eSeconds = chrono::duration_cast<chrono::duration<int>>(timeStart - timeNow).count();
+
+	} while (eSeconds < 10); // 10sec timer
+
+}
 
 Player::~Player()
 {
--- Player.h
+++ Player.h
@@ -1,8 +1,25 @@
 #pragma once
+#include <string>
+#include <chrono>
+#include "Board.h"
+#include <conio.h>
+
+using std::string;
+
 class Player
 {
 public:
 	Player();
+	Player(char side, string name, bool humanP);
 	~Player();
+
+	void makeTurn(Board &board);
+	
+private:
+	void humanTurn(Board &board);
+	void cpuTurn(Board &board);
+	string name;
+	char player;
+	bool human;
 };
 
